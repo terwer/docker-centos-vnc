9e4942847dafae7089bc2cef17c6f905bdb3fb7d
Terwer <youweics@sina.com>
2019-04-11 18:58:57 +0800

    VNC connected with grey screen

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
+# 关闭VNC
+chmod 777 /tmp
+rm /tmp/.X1-lock
+if [ -d ~/.vnc ]; then
+ vncserver -kill :1
+ rm ~/.vnc/*.pid
+ rm ~/.vnc/*.log
+fi
 
+# 启动VNCServer
+# vncserver :1 -geometry 800x600 -depth 24 -SecurityTypes None
+vncserver -SecurityTypes None
 
 echo "finished."
 bash
